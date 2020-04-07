742cc11fae383316fd88038bb871bd51fd194c5b
Julian Camilo Builes Serrano <buiser147@hotmail.com>
2020-04-06 20:23:19 -0500

    it all works

--- k-Means.cxx
+++ k-Means.cxx
@@ -1,37 +1,52 @@
 #include <iostream>
 #include <itkImage.h>
 #include <itkImageFileReader.h>
-#include <itkScalarImageKmeansImageFiltrer.h>
+#include <itkImageFileWriter.h>
+#include <itkScalarImageKmeansImageFilter.h>
 
-typedef itk::Image<short, Dim > TImage;
-static void CreateImage(ImageType::Pointer image);
-int main(int arg, char* argv[])
+const unsigned int  Dim = 2;
+typedef short TPixel ;
+typedef unsigned char TLabel; 
+
+typedef itk::Image< TPixel, Dim > TImage;
+typedef itk::Image< TLabel, Dim > TLabels;
+int main(int argc, char* argv[])
 {
+	if(argc < 4)
+	{
+		std::cerr
+		      << "Usage: " << argv[ 0 ] << "input output k1 k2 k3 ..." << std::endl;
+			
+	}
 	 std::string input_filename = argv[ 1 ];
 	 std::string output_filename = argv[ 2 ];
 
 	 //we set the image filtrer reder as TReader
-	 typedef itk::ImagefFileReader< TImage> TReader;
+	 typedef itk::ImageFileReader< TImage> TReader;
 	 //create the pointer 
-	 TReader::Pointer reader = TReader::new( );
+	 TReader::Pointer reader = TReader::New( );
 	 //set the file to get it like a image 
 	 reader->SetFileName( input_filename );
-	 typedef itk::ScalarImagekmeansFiltrer< TImage , TLabel> TKmeans;
-	 TKmeans:: Pointer kmeans = TKmeans::new( );
+
+	 typedef itk::ScalarImageKmeansImageFilter< TImage , TLabels> TKmeans;
+	 TKmeans::Pointer kmeans = TKmeans::New( );
 	 kmeans->SetInput( reader->GetOutput( ) );
-	 typedef:: itk::ImageFileWriter< TKmeans > TWriter;
-	 TWriter::Pointer writer = TWriter::new( );
+	   for( int i = 3; i < argc; ++i )
+    		kmeans->AddClassWithInitialMean( std::atof( argv[ i ] ) );
+
+	 typedef:: itk::ImageFileWriter< TLabels > TWriter;
+	 TWriter::Pointer writer = TWriter::New( );
 	 writer->SetInput(  kmeans->GetOutput( ) );
 	 writer->SetFileName( output_filename );
 
 	 try
 	 {
-	 	writer->Update;
+	 	writer->Update( );
 
 	 } 
 	 catch(std::exception& err)
 	 {
-	 	std::cerr<<"Error caught: "<<err.what( )<<std:endl;
+	 	std::cerr<<"Error caught: "<<err.what( )<<std::endl;
 	 	return(EXIT_FAILURE);
 	 }
 	 return(EXIT_SUCCESS);
